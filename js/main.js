ae500bb0ac8f3a256024782d1cc75352d88dbc10
blckclov3r <blckclov3r@gmail.com>
2019-09-08 16:41:30 -0800

    slickjs

--- js/main.js
+++ js/main.js
@@ -40,9 +40,42 @@ $(document).ready(function(){
 	  slidesToShow: 4,
 	  slidesToScroll: 1,
 	  autoplay: true,
-	  autoplaySpeed: 2000,
+	   centerPadding: '30px',
+	   adaptiveHeight: true,
+	
+	  autoplaySpeed: 2800,
 	  nextArrow: $('.next'),
 	  prevArrow: $('.prev'),
+	   responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 2,
+	        infinite: true,
+	        dots: false
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
 	});
+
+	
 		
 });
